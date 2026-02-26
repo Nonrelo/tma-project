38f9128f6a0722f76798f43fb120ef58cbd6b2c2
Nonrelo <81495040+Nonrelo@users.noreply.github.com>
2026-02-27 02:40:40 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,9 +9,10 @@ import adminRouter from './routes/admin';
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(helmet());
 
-// Полностью открытый CORS
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
